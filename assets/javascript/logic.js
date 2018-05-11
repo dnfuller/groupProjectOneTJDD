ac93e10a31165853bc032833284d3045dc9bf03e
Tyler Zonies <tylerzonies@gmail.com>
2018-05-11 13:26:07 -0600

    login html fixed and js added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,38 @@
-a
+var config = {
+    apiKey: "AIzaSyAk6IGsE44hQcZ0sPeGHenFFd-r4wtbOwQ",
+    authDomain: "groupprojectonetjdd.firebaseapp.com",
+    databaseURL: "https://groupprojectonetjdd.firebaseio.com",
+    projectId: "groupprojectonetjdd",
+    storageBucket: "groupprojectonetjdd.appspot.com",
+    messagingSenderId: "302849684174"
+  };
+  firebase.initializeApp(config);
+
+  var auth = firebase.auth();
+  var database = firebase.database();
+
+$("#loginSubmit").on("click", function(event){
+    event.preventDefault();
+    var email = $("#emailLogin").val().trim();
+    var password = $("#passwordLogin").val().trim();
+    auth.signInWithEmailAndPassword(email, password).catch(function(e){
+        console.log(e.code);
+    }).then(function(){
+        // console.log("logged in");
+    })
+    $("#emailLogin").val("");
+    $("#passwordLogin").val("");
+})
+$("#logout").on("click", function(event){
+    event.preventDefault();
+    auth.signOut().then(function(){
+        // console.log("signed out");
+    });
+})
+auth.onAuthStateChanged(function(user){
+    if(user){
+        console.log(user.uid);
+    }else{
+        console.log("signed Out.")
+    }
+})
