ea1e7dcd0f7ef82a7bf3aadcd5ef43148a2bcbc4
Tyler Zonies <tylerzonies@gmail.com>
2018-05-13 17:13:54 -0600

    have user auth working and creating a new folder in database

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,16 +10,18 @@ var config = {
 
   var auth = firebase.auth();
   var database = firebase.database();
+  var currentUserID;
 
 $("#loginSubmit").on("click", function(event){
     event.preventDefault();
     var email = $("#emailLogin").val().trim();
     var password = $("#passwordLogin").val().trim();
     auth.signInWithEmailAndPassword(email, password).catch(function(e){
-        console.log(e.code);
+        console.log(e.message);
     }).then(function(){
         // console.log("logged in");
-    })
+    });
+    writeUserData()
     $("#emailLogin").val("");
     $("#passwordLogin").val("");
 })
@@ -29,10 +31,49 @@ $("#logout").on("click", function(event){
         // console.log("signed out");
     });
 })
+$("#createSubmit").on('click', function(event){
+    event.preventDefault();
+    var name = $("#nameCreate").val();
+    var email = $("#emailCreate").val();
+    var password = $("#passwordCreate").val();
+    var passwordConfirm = $("#passwordCreateConfirm").val();
+    var failed = false;
+
+    if(password === passwordConfirm){
+        auth.createUserWithEmailAndPassword(email, password).catch(function(e){
+            // if(e.message == "aut
+            M.toast({html: e.message});
+            failed = true;
+        }).then(function(){
+            if(!failed){
+                writeUserData(firebase.auth().currentUser.uid, name, email);
+                console.log(firebase.auth().currentUser.uid + " create");
+            }
+            
+        })
+        
+    }else{
+        M.toast({html: 'password did not match confirm'});
+    }
+})
 auth.onAuthStateChanged(function(user){
     if(user){
         console.log(user.uid);
+        currentUserID = user.uid;
+        // var currentUser = 
+        // database.ref("users").child(user.uid).setValue("users");
+        
     }else{
         console.log("signed Out.")
     }
-})
+});
+function writeUserData(userId, name, email) {
+    firebase.database().ref('users/' + userId).set({
+      name: name,
+      email: email,
+      range: "",
+      isOwner: false,
+      dogs: ""
+        
+    });
+  }
